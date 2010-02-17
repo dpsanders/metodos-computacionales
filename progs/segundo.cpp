de379cfc8bacaf69a185b53e458a7730812eedde
David P. Sanders <dpsanders@gmail.com>
2010-02-17 10:31:33 -0600

    Agregue las versiones LaTeX de los primeros 5 capitulos adentro del directorio 'notas', y algunos programas en 'progs'

---
+++ progs/segundo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	double temp, presion;
+	
+	cin >> temp >> presion;
+	
+	cout << "Usando temp=" << temp << "; presion=" << presion << endl;
+	
+	return 0;	    // Regresar un valor al mundo externo
+}
