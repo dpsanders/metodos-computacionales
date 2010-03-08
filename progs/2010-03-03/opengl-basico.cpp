5e907c3d56debf13eebb9a43d6b1fed79680ddee
David Sanders <dsanders@dsanders-netbook.(none)>
2010-03-08 11:46:08 -0600

    Agregue progs de 2010-03-03

--- progs/2010-03-03/opengl-basico.cpp
+++ progs/2010-03-03/opengl-basico.cpp
@@ -1,23 +1,44 @@
 // OpenGL y GLUT basicos
 // Compilar con la opcion -lglut  para ligar con la libreria de GLUT
 
+// Compilar con
+// -framework OpenGL -framework GLUT   en Mac
+
 #include <GL/glut.h>
+//#include <GLUT/glut.h>  // MAC
 #include <iostream>
 
 using namespace std;
 
 void display() {
     
-    glClear(GL_COLOR_BUFFER_BIT);        // limpiar pantalla
-    glBegin(GL_TRIANGLES);                      // prepararse para dibujar un triangulo
+	static float desplazamiento = 0.0;
+	
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);        
+	// limpiar pantalla
     
+	glPushMatrix();
+	glTranslatef(desplazamiento, 0., 0.);
+	
+	glBegin(GL_POLYGON);                      // prepararse para dibujar un triangulo
+    
+	
+	glColor3f(1., 0., 0.);
+	
     glVertex2f(0.0,0.0);                                // especificar un vértice en 2D, que consiste en números flotatntes
     glVertex2f(0.5,0.0);
     glVertex2f(0.5,0.5);
+	glVertex2f(1.0, 1.0);
     
     glEnd();                                                // terminar de dibujar el triángulo
     glFlush();                                              // mandar los comandos ya
     
+	glPopMatrix();
+	
+	desplazamiento += 0.001;
+	if (desplazamiento > 1)
+		desplazamiento = 0.0;
+// 	glutSwapBuffers();
 }
 
 int main(int argc, char** argv) {
@@ -30,8 +51,11 @@ int main(int argc, char** argv) {
     glutInitWindowPosition(200,200);
     glutCreateWindow("Primero");    // se puede dar un título a la ventana que se crea
     
+	
+	
     glutDisplayFunc(display);           // decirle a glut cual función usar para desplegar 
     
+	glutIdleFunc(display);
     glutMainLoop();                         // entrar en el bucle principal de glut
     
 }
